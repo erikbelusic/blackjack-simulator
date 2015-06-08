caa7abc4868e11050a759e8af1f0e8583bdc1e9a
Erik Belusic <erikbelusic@Eriks-Air.home>
2015-06-08 00:13:18 -0400

    implement get total and get hand type

--- classes/Hand.php
+++ classes/Hand.php
@@ -11,7 +11,56 @@ class Hand
 
     public function getTotal()
     {
-        // TODO: implement
+        $total = 0;
+        $aces = [];
+
+        foreach ($this->cards as $card)
+        {
+            $value = $card->getValue();
+            if (count($value) == 1)
+            {
+                $total += $value;
+            }
+            else
+            {
+                array_push($aces, $value);
+            }
+        }
+
+        foreach ($aces as $ace)
+        {
+            if ($total + $ace[1] <= 21)
+            {
+                $total += $ace[1];
+            }
+            else
+            {
+                $total += $ace[0];
+            }
+        }
+
+        return $total;
+    }
+
+    public function getHandType()
+    {
+        if (count($this->cards) == 2 && $this->cards[0]->getValue() == $this->cards[1]->getValue())
+        {
+            return BlackJackGame::HAND_SPLITTABLE;
+        }
+
+        $cardRanks = [];
+        foreach ($this->cards as $card)
+        {
+            array_push($cardRanks, $card->getRank());
+        }
+
+        if (in_array('ace', $cardRanks))
+        {
+            return BlackJackGame::HAND_SOFT;
+        }
+
+        return BlackJackGame::HAND_HARD;
     }
 
     public function getCards()
