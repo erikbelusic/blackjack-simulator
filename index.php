0f813915c484ad384f761710442809495454edb5
Erik Belusic <erikbelusic@Eriks-Air.home>
2015-06-08 00:14:02 -0400

    create and test lookup for basic strategy

--- index.php
+++ index.php
@@ -2,5 +2,11 @@
 
 require __DIR__ . '/vendor/autoload.php';
 
-$game = new BlackJackGame();
-$game->start();
+//$game = new BlackJackGame();
+//$game->start();
+
+$strategy = new FirstPlayerStrategy();
+$myArray = $strategy->getStrategyArray();
+dump($myArray[BlackJackGame::HAND_HARD][12][2]);
+dump($myArray[BlackJackGame::HAND_HARD][12][3]);
+dump($myArray[BlackJackGame::HAND_HARD][12][4]);
